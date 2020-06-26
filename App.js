ef8ef3266ec413db289c263fa2838a53c76fb552
Luis Alvarez <lealvarezp@unac.edu.co>
2020-06-25 22:27:06 -0500

    form listo

--- App.js
+++ App.js
@@ -1,17 +1,96 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import Formulario from "./components/Formulario";
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+} from 'react-native';
+import Formulario from './components/Formulario';
+import {Overlay, Divider} from 'react-native-elements';
 
 const App = () => {
-	return (
-		<>
-			<Text>CLIMA</Text>
+  const [search, setSearch] = useState({
+    city: '',
+    country: '',
+  });
+  const [visible, setVisible] = useState(false);
+  const [result, setResult] = useState({});
+  const {city, country} = search;
+  const [consulted, setConsulted] = useState(false);
 
-			<Formulario />
-		</>
-	);
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+  useEffect(() => {
+    const consultWeather = async () => {
+      if (consulted) {
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=89919db9df3608f8a06ba8185d072b0d`;
+        try {
+          const response = await fetch(url);
+          const apiResult = await response.json();
+          setResult(apiResult);
+          setConsulted(false);
+        } catch (err) {
+          toggleOverlay();
+        }
+      }
+    };
+    consultWeather();
+  }, [consulted]);
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={styles.app}>
+          <View>
+            <View style={styles.content}>
+              <Formulario
+                search={search}
+                setSearch={setSearch}
+                setConsulted={setConsulted}
+              />
+            </View>
+          </View>
+          <Overlay
+            isVisible={visible}
+            onBackdropPress={toggleOverlay}
+            overlayStyle={{width: '85%', alignSelf: 'center'}}>
+            <View>
+              <Text style={{color: '#ff0000', fontSize: 20}}>ERROR!!</Text>
+              <Text style={{fontSize: 15, marginBottom: 2}}>
+                Something went wrong, try with another city or country
+              </Text>
+              <Divider style={{backgroundColor: 'black'}} />
+              <TouchableOpacity onPress={() => toggleOverlay()}>
+                <Text
+                  style={{
+                    color: '#3dabff',
+                    alignSelf: 'center',
+                    fontSize: 15,
+                    marginRight: 5,
+                    marginTop: 2,
+                  }}>
+                  OK
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Overlay>
+        </View>
+      </TouchableWithoutFeedback>
+    </>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    marginHorizontal: '2.5%',
+  },
+  app: {
+    flex: 1,
+    backgroundColor: 'rgb(71,149,212)',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
--- components/Formulario.js
+++ components/Formulario.js
@@ -1,35 +1,149 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
-  ScrollView,
-  TextInput,
   TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+  Animated,
 } from 'react-native';
-import {Input} from 'react-native-elements';
+import {Input, Overlay, Divider} from 'react-native-elements';
 import Icon from 'react-native-ionicons';
+import {Picker} from '@react-native-community/picker';
+const Formulario = ({search, setSearch, setConsulted}) => {
+  const {city, country} = search;
+  const [btnAnimation] = useState(new Animated.Value(1));
+  const [visible, setVisible] = useState(false);
 
-const Formulario = () => {
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
+  const getWeather = () => {
+    if (country.trim() === '' || city.trim() === '') {
+      toggleOverlay();
+      return;
+    }
+
+    setConsulted(true);
+  };
+
+  const animationIn = () => {
+    Animated.spring(btnAnimation, {
+      toValue: 0.8,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const animationOut = () => {
+    Animated.spring(btnAnimation, {
+      toValue: 1,
+      friction: 4,
+      tension: 30,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const animationStyle = {transform: [{scale: btnAnimation}]};
+
   return (
     <>
-      <TouchableWithoutFeedback>
-        <View style={styles.formulario}>
-          <View>
-            <Input
-              placeholder="Ciudad"
-              leftIcon={<Icon name="globe" color="black" />}
-            />
-          </View>
+      <View>
+        <View>
+          <Input
+            value={city}
+            onChangeText={(city) => setSearch({...search, city})}
+            inputContainerStyle={{backgroundColor: '#eee'}}
+            placeholder="City"
+            leftIcon={<Icon name="globe" color="black" />}
+          />
         </View>
-      </TouchableWithoutFeedback>
+        <View>
+          <Picker
+            style={styles.picker}
+            selectedValue={country}
+            onValueChange={(country) => {
+              setSearch({...search, country});
+            }}>
+            <Picker.Item label="-- Select country --" value="" />
+            <Picker.Item label="United States of America" value="US" />
+            <Picker.Item label="United Kindom" value="UK" />
+            <Picker.Item label="Brazil" value="BR" />
+            <Picker.Item label="Germany" value="DE" />
+            <Picker.Item label="Ireland" value="IR" />
+            <Picker.Item label="Japan" value="JP" />
+            <Picker.Item label="Mexico" value="MX" />
+            <Picker.Item label="Spain" value="ES" />
+            <Picker.Item label="Venezuela" value="VE" />
+            <Picker.Item label="France" value="FR" />
+            <Picker.Item label="Colombia" value="CO" />
+            <Picker.Item label="Italy" value="IT" />
+            <Picker.Item label="Portugal" value="PT" />
+          </Picker>
+        </View>
+
+        <TouchableWithoutFeedback
+          onPressIn={() => animationIn()}
+          onPressOut={() => animationOut()}
+          onPress={() => {
+            getWeather();
+          }}>
+          <Animated.View style={[styles.btnWeather, animationStyle]}>
+            <Text
+              style={{
+                fontSize: 20,
+                color: '#fff',
+                alignSelf: 'center',
+              }}>
+              Get Weather
+            </Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
+
+        <Overlay
+          isVisible={visible}
+          onBackdropPress={toggleOverlay}
+          overlayStyle={{width: '85%', alignSelf: 'center'}}>
+          <View>
+            <Text style={{color: '#ff0000', fontSize: 20}}>ERROR!!</Text>
+            <Text style={{fontSize: 15, marginBottom: 2}}>
+              You need a CITY and COUNTRY to get the weather
+            </Text>
+            <Divider style={{backgroundColor: 'black'}} />
+            <TouchableOpacity onPress={() => toggleOverlay()}>
+              <Text
+                style={{
+                  color: '#3dabff',
+                  alignSelf: 'center',
+                  fontSize: 15,
+                  marginRight: 5,
+                  marginTop: 2,
+                }}>
+                OK
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Overlay>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  formulario: {
-    marginTop: 100,
+  picker: {
+    width: '80%',
+    alignSelf: 'center',
+    backgroundColor: '#fff',
+    width: 320,
+  },
+  btnWeather: {
+    backgroundColor: '#000',
+    borderRadius: 8,
+    width: 300,
+    padding: 5,
+    alignSelf: 'center',
+    marginVertical: 50,
   },
 });
 
